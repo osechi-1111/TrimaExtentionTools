af50e816eabee7fdf49ea0b7cfb07c8e180e2626
osechi-1111 <121627718+osechi-1111@users.noreply.github.com>
2023-11-21 21:58:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let resultSpan = document.getElementById('resultSpan');
+let errorMessage = document.getElementById('errorMessage');
+let errorTitle = document.getElementById('errorTitle');
+let errorContent = document.getElementById('errorContent');
+let userRankSelect = document.getElementById('userRankSelect');
 
 function get_number(btn){
   let resultValue = document.getElementById('resultValue');
   let resultValueElement = parseInt(resultValue.value);
-  let result = 50 - resultValueElement / 60;
+  let userRankValue = userRankSelect.value;
+  let result = 50 - resultValueElement / userRankValue;
   if(result < 0){
     result = 0;
   }
   if(btn.value == '計算'){
     if(Number.isInteger(result)){
       resultSpan.innerText = result;
+      errorMessage.style.display = 'none';
     }else{
-      console.log("計算不可能")
+      errorMessage.style.display = 'block';
+      errorTitle.innerText = 'エラー：マイル入力数の誤り';
+      errorContent.innerText = `　数値が${userRankValue}で割り切れない、もしくは整数ではない可能性があります。マイル数を正しく入力し直して下さい。`;　
+      resultSpan.innerText = '-';
     }
   }else{
     if(btn.value == '⇦'){
